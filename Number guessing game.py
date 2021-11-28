39ddfc741755f326be845f74b0339ea75edb7b3a
NormanMueller <93076064+NormanMueller@users.noreply.github.com>
2021-11-28 22:41:10 +0100

    initial commit

---
+++ Number guessing game.py
@@ -0,0 +1,33 @@
+from random import randint
+
+def guessing_game ():
+     
+    guessing_number = int(randint(0, 10))
+
+
+    def get_user_input():
+        
+        user_input = int(input('Whats your number?'))
+        return user_input
+
+    def evaluate_user_input(user_input, guessing_number) : 
+        
+        if user_input > guessing_number: 
+            return 'your number os to high'
+        
+        elif user_input < guessing_number: 
+            return 'your number os to low'
+
+        elif user_input == guessing_number: 
+            return (f'you have the rigth number , the number is {user_input}') 
+
+
+
+    while True :
+        
+        user_input = get_user_input()
+        eval = evaluate_user_input(user_input, guessing_number)
+        print(eval)
+                
+guessing_game ()
+
