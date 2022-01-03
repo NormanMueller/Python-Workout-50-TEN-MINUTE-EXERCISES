ca19f935d99f35ab7e96205b02954c71c0e92171
NormanMueller <93076064+NormanMueller@users.noreply.github.com>
2022-01-03 22:22:24 +0100

    clean

--- Pig Latin sentence.py
+++ Pig Latin sentence.py
@@ -1,17 +1,17 @@
-def pig_latin(string):
-
-    if string[0] in 'aeiou':
-        return string + 'way'
-    else :
-        first_letter = string[0] 
-        new_string = string[1:] + first_letter + 'ay'
-        return new_string
-
-
 def pig_latin_sentence (sentence):
     
+    def pig_latin(string):
+
+        if string[0] in 'aeiou':
+            return string + 'way'
+        else :
+            first_letter = string[0] 
+            new_string = string[1:] + first_letter + 'ay'
+            return new_string
+
     word_list = sentence.split()
     pig_latin_sentence = []
+    
     for word in word_list:
         pig_latin_sentence.append(pig_latin(word))
     
